cc4f059a4bc016d7faae11c913de4ec6446578f0
League Student <league@iMac.attlocal.net>
2019-05-12 11:27:06 -0700

    OFFICE CATS
    :laughing: :smile:

--- src/elseif/CrazyCatLady.java
+++ src/elseif/CrazyCatLady.java
@@ -5,18 +5,27 @@ package elseif;
 
 import java.net.URI;
 
+import javax.swing.JOptionPane;
+
 public class CrazyCatLady {
 	public static void main(String[] args) {
 		// 1. Ask the user how many cats they have
-
+		String what =
+JOptionPane.showInputDialog("How many cats do you have?");
 		// 2. Convert their answer into an int
-
+int bob = Integer.parseInt(what);
 		// 3. If they have 3 or more cats, tell them they are a crazy cat lady
-
+if(bob>= 3) {
+	JOptionPane.showMessageDialog(null, "You're a crazy cat lady");
+}
 		// 4. If they have less than 3 cats AND more than 0 cats, call the method below to show them a cat video
-
+if(bob<3&&bob>0) {
+	playVideo("https://www.youtube.com/watch?v=vASw0m6YdWs");
+}
 		// 5. If they have 0 cats, show them a video of A Frog Sitting on a Bench Like a Human
-		
+		if(bob==0) {
+			playVideo("https://www.youtube.com/watch?v=oj_yLBltPE8");
+		}
 	}
 
 	static void playVideo(String videoURL) {
--- src/elseif/RobotColorChooser.java
+++ src/elseif/RobotColorChooser.java
@@ -4,24 +4,51 @@
 package elseif;
 
 import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 import java.awt.Color;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
+		
 		//1. Create a new Robot
-		
+		Robot bob=new Robot();
+		for (int i = 0; i < 30; i++) {
+			
+		}
 		//3. Ask the user what color they would like the robot to draw
-		
+		String hi =
+		JOptionPane.showInputDialog("What color would you like the robot to use?");
 		//5. Use an if/else statement to set the pen color that the user requested
-
-        //6. If the user doesn’t enter anything, choose a random color
-
+if(hi.equals("red")) {
+	bob.setPenColor(255, 0, 0);
+}
+if(hi.equals("green")) {
+	bob.setPenColor(0, 255, 0);
+}
+        //6. If the user doesn’t enter anything, choose a dark color
+//already did it!
         //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
 		
 		//4. Set the pen width to 10
-		
-	    //2. Make the robot draw a shape (this will take more than one line of code)
+		bob.setPenWidth(10);
+	    //2. Make the robot draw a shape (this will take more than one line of
+ bob.hide(); 
+ 
+ 
+ bob.setSpeed(100); 
+ 
+ 
+ bob.penDown(); 
+ 
+ for (int i = 0; i < 4; i++) {
+	
 
-
+ bob.move(200);
+ 
+ 
+ bob.turn(90); 
 	}
+}
 }
