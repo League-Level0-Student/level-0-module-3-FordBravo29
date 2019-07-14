ab02a0bb708f784603b4c769e50ad9c3e1572fb6
League Student <league@iMac-24.attlocal.net>
2019-07-14 10:53:16 -0700

    hi

---
+++ src/CheckPoint3.java
@@ -0,0 +1,26 @@
+import javax.swing.JOptionPane;
+
+public class CheckPoint3 {
+public static void main(String[] args) {
+String spud="potato";	
+	
+	
+	for(int i = 1; i < 8; i++) {
+		if(i==(4)) {
+			JOptionPane.showMessageDialog(null, i);;
+		}
+		else {
+			JOptionPane.showMessageDialog(null, i  +  spud );	
+		}
+	}
+	JOptionPane.showMessageDialog(null, "more");
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
